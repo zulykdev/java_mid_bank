1012eb4dab151ebb03989077f7f3eb8fbc91353a
Yoselin <yrudasb@gmail.com>
2024-04-22 22:58:44 -0500

    ingreso de los atributos de cta ahorro, cta credito y empleado

--- src/main/java/com/brixton/bank/model/Employee.java
+++ src/main/java/com/brixton/bank/model/Employee.java
@@ -1,4 +1,27 @@
 package com.brixton.bank.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Employee {
+    private String idEmployee;
+    private String name;
+
+
+    public void aprobarPrestamo(){
+
+    }
+
+    public void rechazarPrestamo(){
+
+    }
+
+    public void gestionarCuentaCliente(){
+
+    }
+
 }
--- src/main/java/com/brixton/bank/model/SavingAccount.java
+++ src/main/java/com/brixton/bank/model/SavingAccount.java
@@ -1,4 +1,16 @@
 package com.brixton.bank.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class SavingAccount extends Account{
+    private  double interes;
+
+    public double calculoInteres (double interes, double monto){
+        return 0.0;
+    }
 }
